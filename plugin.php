29b6f9fc3e343449497691ee875536522ab35141
Ren <s0985514623@gmail.com>
2025-06-16 11:29:13 +0800

    chore: release v1.0.34

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Wp Tinwing
  * Plugin URI:        wp-tinwing
  * Description:       WP版本-天永保險系統
- * Version:           1.0.33
+ * Version:           1.0.34
  * Requires at least: 5.7
  * Requires PHP:      8.0
  * Author:            Ren
