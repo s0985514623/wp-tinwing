d8037259d8501bd273885dbe345a78907ae8d70f
Ren <s0985514623@gmail.com>
2025-06-17 14:23:41 +0800

    fix: 更新EditMetaMotor组件中的保险费用计算逻辑，确保使用grossPremium而非watchPremium进行计算

--- js/src/pages/debitNotes/components/EditMetaMotor.tsx
+++ js/src/pages/debitNotes/components/EditMetaMotor.tsx
@@ -22,7 +22,7 @@ const EditMetaMotor = () => {
         ls: watchLs,
         ncb: watchNcb,
     });
-
+    
     const mibValue = round(grossPremium * (watchMib / 100), 2);
 
     const totalPremium = getMotorTotalPremium({
@@ -39,7 +39,7 @@ const EditMetaMotor = () => {
     // const insurerFee = round(grossPremium * (watchInsurerFeePercent / 100), 2);
     const insurerTotalFee = mibValue + watchExtraValue + round(grossPremium * (watchInsurerFeePercent / 100), 2);
     useEffect(() => {
-        setInsurerFee(round(watchPremium * (watchInsurerFeePercent / 100),2));
+        setInsurerFee(round(grossPremium * (watchInsurerFeePercent / 100),2));
     }, [watchInsurerFeePercent]);
 
     const handleChange = (value: number | null) => {
