9a0712e9d8fee6d63ee1d07e824e3d8cb4128c27
Ren <s0985514623@gmail.com>
2025-08-31 21:04:23 +0800

    feat: 更新 OtherReport 類別以支持未支付保險的查詢，並新增計算保險支付的功能

--- inc/classes/Api/OtherReport.php
+++ inc/classes/Api/OtherReport.php
@@ -6,9 +6,10 @@
 declare (strict_types = 1);
 
 namespace J7\WpTinwing\Api;
-use J7\WpTinwing\Plugin;
+
 use J7\WpUtils\Classes\WP;
 use J7\WpTinwing\Utils\Base;
+use J7\WpTinwing\Plugin;
 /**
  * Class Entry
  */
@@ -230,6 +231,7 @@ final class OtherReport
     }
     /**
      * Get insurer_ageing_report callback
+     * 找出還沒收到錢的insurer = receipt.is_paid = false
      *
      * @param \WP_REST_Request $request Request.
      * @return \WP_REST_Response
@@ -238,10 +240,9 @@ final class OtherReport
     {
         $params = $request->get_query_params() ?? [];
         $params = WP::sanitize_text_field_deep($params, false);
-        // error_log(print_r($params, true));
-        // 查詢 Custom Post Type 'debit_notes' 和 'credit_notes' 的文章
+        // 查詢 Custom Post Type 'receipts' 的文章
         $args = [
-            'post_type'      => ['debit_notes', 'credit_notes'],                                   // 自定義文章類型名稱
+            'post_type'      => ['receipts'],                                                      // 自定義文章類型名稱
             'posts_per_page' => isset($params['posts_per_page']) ? $params['posts_per_page'] : -1, // 每頁顯示文章數量
             'paged'          => isset($params['page']) ? $params['page'] : 1,                      // 當前頁碼
             'orderby'        => isset($params['orderby']) ? $params['orderby'] : 'id',             // 排序方式
@@ -252,30 +253,21 @@ final class OtherReport
             $meta_query         = Base::sanitize_meta_query($params['meta_query']);
             $args['meta_query'] = $meta_query;
         }
-        //加入排除條件,receipt_id不存在或為空
+        //加入排除條件,is_paid = 0
         $args['meta_query'][] = [
-            'key'     => 'receipt_id',
-            'value'   => '',
-            'compare' => 'NOT EXISTS',
+            'key'     => 'is_paid',
+            'compare' => '=',
+            'value'   => '0',
         ];
         // error_log(print_r($args, true));
         //主查詢
         $query = new \WP_Query($args);
-        //取得insurer ids資料
-        $insurer_ids = [];
-        if ($query->have_posts()) {
-            while ($query->have_posts()) {
-                $query->the_post();
-                $insurer_ids[] = get_post_meta(get_the_ID(), 'insurer_id', true);
-            }
-        }
-        $insurer_ids = array_values(array_unique($insurer_ids));
+
         //取得insurer 資料
         $insurer_map = [];
-        if ($insurer_ids) {
+        if ($query->have_posts()) {
             $insurer_data = get_posts([
                 'post_type'              => 'insurers',
-                'post__in'               => $insurer_ids,
                 'update_post_meta_cache' => true,
                 'fields'                 => 'all',
                 'numberposts'            => -1,
@@ -284,34 +276,112 @@ final class OtherReport
                 $insurer_map[$insurer->ID] = $insurer;
             }
         }
+        //取得debit_note ids資料
+        $debit_note_ids = [];
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                $debit_note_ids[] = get_post_meta(get_the_ID(), 'debit_note_id', true);
+            }
+        }
+        $debit_note_ids = array_values(array_unique($debit_note_ids));
+        //取得debit_note 資料
+        $debit_note_map = [];
+        if ($debit_note_ids) {
+            $debit_note_data = get_posts([
+                'post_type'              => 'debit_notes',
+                'post__in'               => $debit_note_ids,
+                'update_post_meta_cache' => true,
+                'fields'                 => 'all',
+                'numberposts'            => -1,
+            ]);
+            foreach ($debit_note_data as $debit_note) {
+                $debit_note_map[$debit_note->ID] = $debit_note;
+            }
+        }
+        //取得credit_note ids資料
+        $credit_note_ids = [];
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                $credit_note_ids[] = get_post_meta(get_the_ID(), 'created_from_credit_note_id', true);
+            }
+        }
+        $credit_note_ids = array_values(array_unique($credit_note_ids));
+        //取得credit_note 資料
+        $credit_note_map = [];
+        if ($credit_note_ids) {
+            $credit_note_data = get_posts([
+                'post_type'              => 'credit_notes',
+                'post__in'               => $credit_note_ids,
+                'update_post_meta_cache' => true,
+                'fields'                 => 'all',
+                'numberposts'            => -1,
+            ]);
+            foreach ($credit_note_data as $credit_note) {
+                $credit_note_map[$credit_note->ID] = $credit_note;
+            }
+        }
+        //取得renewal ids資料
+        $renewal_ids = [];
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                $renewal_ids[] = get_post_meta(get_the_ID(), 'created_from_renewal_id', true);
+            }
+        }
+        $renewal_ids = array_values(array_unique($renewal_ids));
+        //取得renewal 資料
+        $renewal_map = [];
+        if ($renewal_ids) {
+            $renewal_data = get_posts([
+                'post_type'              => 'renewals',
+                'post__in'               => $renewal_ids,
+                'update_post_meta_cache' => true,
+                'fields'                 => 'all',
+                'numberposts'            => -1,
+            ]);
+            foreach ($renewal_data as $renewal) {
+                $renewal_map[$renewal->ID] = $renewal;
+            }
+        }
         // 整合資料
         $posts_data = [];
         if ($query->have_posts()) {
-            $total_premium = 0;
+            $total_insurer_payment = 0;
             while ($query->have_posts()) {
                 $query->the_post();
-                $insurer = $insurer_map[get_post_meta(get_the_ID(), 'insurer_id', true)];
+                $debit_note  = $debit_note_map[get_post_meta(get_the_ID(), 'debit_note_id', true)];
+                $credit_note = $credit_note_map[get_post_meta(get_the_ID(), 'created_from_credit_note_id', true)];
+                $renewal     = $renewal_map[get_post_meta(get_the_ID(), 'created_from_renewal_id', true)];
+                $the_note    = $credit_note ?? $renewal ?? $debit_note;
+                $insurer     = $insurer_map[get_post_meta($the_note->ID, 'insurer_id', true)];
                 //如果沒有insurer,則跳過
                 if (! $insurer) {
                     continue;
                 }
-                $insurer_id = $insurer ? $insurer->ID : '';
-                $premium    = (float) (get_post_meta(get_the_ID(), 'premium', true) ?: 0);
-                $total_premium += $premium;
+                $insurer_id      = $insurer ? $insurer->ID : '';
+                $insurer_payment = $this->get_insurer_payment($the_note, $insurer);
+                if ($credit_note) {
+                    $insurer_payment = -$insurer_payment;
+                }
+                $total_insurer_payment += $insurer_payment;
+                // error_log('get_the_ID():'.get_the_ID());
+                // error_log('insurer_payment:'.$insurer_payment);
                 //如果$posts_data[$insurer_id]存在,則累加'120Days & Over'與'Current Balance'
                 if (isset($posts_data[$insurer_id])) {
-                    $posts_data[$insurer_id]['120Days & Over'] += $premium;
-                    $posts_data[$insurer_id]['Current Balance'] += $premium;
+                    $posts_data[$insurer_id]['120Days & Over'] += $insurer_payment;
+                    $posts_data[$insurer_id]['Current Balance'] += $insurer_payment;
                 } else {
                     $posts_data[$insurer_id] = [
                         'A/C No'          => $insurer ? get_post_meta($insurer->ID, 'insurer_number', true) : '',
                         'Type'            => '',
                         'Creditor Name'   => $insurer ? $insurer->post_title : '',
-                        '120Days & Over'  => $premium,
+                        '120Days & Over'  => $insurer_payment,
                         '90Days & Over'   => '',
                         '60Days & Over'   => '',
                         '30Days & Over'   => '',
-                        'Current Balance' => $premium,
+                        'Current Balance' => $insurer_payment,
                     ];
                 }
             }
@@ -319,11 +389,11 @@ final class OtherReport
                 'A/C No'          => '',
                 'Type'            => '',
                 'Creditor Name'   => 'Total',
-                '120Days & Over'  => $total_premium,
+                '120Days & Over'  => $total_insurer_payment,
                 '90Days & Over'   => '',
                 '60Days & Over'   => '',
                 '30Days & Over'   => '',
-                'Current Balance' => $total_premium,
+                'Current Balance' => $total_insurer_payment,
             ];
             $posts_data = array_values($posts_data);
             foreach ($posts_data as $key => $value) {
@@ -374,22 +444,21 @@ final class OtherReport
             ];
         }
         //加入payment_status條件
-        if (isset($params['payment_status'])&&$params['payment_status']=='unpaid') {
+        if (isset($params['payment_status']) && $params['payment_status'] == 'unpaid') {
             //加入排除條件,receipt_id不存在或為空
             $args['meta_query'][] = [
                 'key'     => 'receipt_id',
                 'value'   => '',
                 'compare' => 'NOT EXISTS',
             ];
-        }
-        elseif(isset($params['payment_status'])&&$params['payment_status']=='paid'){
+        } elseif (isset($params['payment_status']) && $params['payment_status'] == 'paid') {
             //加入條件,receipt_id存在
             $args['meta_query'][] = [
                 'key'     => 'receipt_id',
                 'compare' => 'EXISTS',
             ];
         }
-        
+
         // error_log(print_r($args, true));
         //主查詢
         $query = new \WP_Query($args);
@@ -469,38 +538,38 @@ final class OtherReport
             $total_premium = 0;
             while ($query->have_posts()) {
                 $query->the_post();
-                $agent     = $agent_map[get_post_meta(get_the_ID(), 'agent_id', true)];
-                $client    = $client_map[get_post_meta(get_the_ID(), 'client_id', true)];
-                $receipt   = $receipt_map[get_post_meta(get_the_ID(), 'receipt_id', true)];
+                $agent        = $agent_map[get_post_meta(get_the_ID(), 'agent_id', true)];
+                $client       = $client_map[get_post_meta(get_the_ID(), 'client_id', true)];
+                $receipt      = $receipt_map[get_post_meta(get_the_ID(), 'receipt_id', true)];
                 $display_name = get_post_meta($client->ID, 'display_name', true);
-                $agent_id  = $agent ? $agent->ID : '';
-                $client_id = $client ? $client->ID : '';
-                $post_type = get_post_type(get_the_ID());
-                $premium   = (float) (get_post_meta(get_the_ID(), 'premium', true) ?: 0);
+                $agent_id     = $agent ? $agent->ID : '';
+                $client_id    = $client ? $client->ID : '';
+                $post_type    = get_post_type(get_the_ID());
+                $premium      = (float) (get_post_meta(get_the_ID(), 'premium', true) ?: 0);
                 $total_premium += $premium;
                 $posts_data[] = [
-                    'Date'            => get_post_meta(get_the_ID(), 'date', true) ? \date_i18n('d/m/y', get_post_meta(get_the_ID(), 'date', true)) : 'N/A',
-                    'Note No'         => get_the_title(),
-                    'Post type'       => $post_type === 'debit_notes' ? 'DN' : 'CN',
-                    'Client Name'     => $client ? get_post_meta($client->ID, $display_name, true) ?? '' : '',
-                    'Premium'         => $premium,
-                    'Agent Code'           => $agent ? get_post_meta($agent->ID, 'agent_number', true) ?? '' : '',
-                    'Receipt No'         => $receipt ? $receipt->post_title : '',
-                    'Payment Date'        => $receipt ? \date_i18n('d/m/y', get_post_meta($receipt->ID, 'payment_date', true)) : '',
+                    'Date'         => get_post_meta(get_the_ID(), 'date', true) ? \date_i18n('d/m/y', get_post_meta(get_the_ID(), 'date', true)) : 'N/A',
+                    'Note No'      => get_the_title(),
+                    'Post type'    => $post_type === 'debit_notes' ? 'DN' : 'CN',
+                    'Client Name'  => $client ? get_post_meta($client->ID, $display_name, true) ?? '' : '',
+                    'Premium'      => $premium,
+                    'Agent Code'   => $agent ? get_post_meta($agent->ID, 'agent_number', true) ?? '' : '',
+                    'Receipt No'   => $receipt ? $receipt->post_title : '',
+                    'Payment Date' => $receipt ? \date_i18n('d/m/y', get_post_meta($receipt->ID, 'payment_date', true)) : '',
                 ];
             }
             $posts_data[] = [
-                'Date'            => '',
-                'Note No'         => '',
-                'Post type'       => '',
-                'Client Name'     => 'Total',
-                'Premium'         => $total_premium,
-                'Agent Code'      => '',
-                'Receipt No'      => '',
-                'Payment Date'    => '',
+                'Date'         => '',
+                'Note No'      => '',
+                'Post type'    => '',
+                'Client Name'  => 'Total',
+                'Premium'      => $total_premium,
+                'Agent Code'   => '',
+                'Receipt No'   => '',
+                'Payment Date' => '',
             ];
             foreach ($posts_data as $key => $value) {
-                $posts_data[$key]['Premium']  = number_format($value['Premium'], 2, '.', ',');
+                $posts_data[$key]['Premium'] = number_format($value['Premium'], 2, '.', ',');
             }
             wp_reset_postdata();
         }
@@ -512,5 +581,38 @@ final class OtherReport
 
         return $response;
     }
-    
+    /**
+     * get_insurer_payment
+     *
+     * @param $the_note
+     * @param $insurer
+     * @return float
+     */
+    public function get_insurer_payment($the_note, $insurer)
+    {
+        $insurer_fee_percent = floatval($the_note->insurer_fee_percent) ?? floatval($insurer->payment_rate) ?? 0;
+        $premium             = floatval($the_note->premium) ?? 0;
+        $motor_attr          = maybe_unserialize(get_post_meta($the_note->ID, 'motor_attr', true));
+        $ls                  = floatval($motor_attr['ls']??0) ?? 0;
+        $ncb                 = floatval($motor_attr['ncb']??0) ?? 0;
+        $gross_premium       = $premium * (1 + $ls / 100) * (1 + $ncb / 100);
+        
+        // 對稱處理，避免負數被系統性往上推
+        $gross_premium       = round($gross_premium + 1e-10, 2,PHP_ROUND_HALF_UP);
+        $template            = $the_note->template ?? '';
+        if ($template == 'motor') {
+            $mib               = floatval($motor_attr['mib']??0) ?? 0;
+            $mib_value         = round($gross_premium * ($mib / 100), 2,PHP_ROUND_HALF_UP);
+            $extra_field       = maybe_unserialize(get_post_meta($the_note->ID, 'extra_field', true));
+            $extra_field_value = $premium * (floatval($extra_field['value']??0) / 100);
+            $insurer_payment   = $mib_value + $extra_field_value + round($insurer_fee_percent * $gross_premium / 100+1e-10, 2,PHP_ROUND_HALF_UP);
+        } else {
+            $levy              = get_post_meta($the_note->ID, 'levy', true);
+            $levy_value        = round($gross_premium * (floatval($levy??0) / 100), 2,PHP_ROUND_HALF_UP);
+            $extra_field       = maybe_unserialize(get_post_meta($the_note->ID, 'extra_field', true));
+            $extra_field_value = $premium * (floatval($extra_field['value']??0) / 100);
+            $insurer_payment   = $levy_value + $extra_field_value + round($insurer_fee_percent * $gross_premium / 100+1e-10, 2,PHP_ROUND_HALF_UP);
+        }
+        return $insurer_payment;
+    }
 }
